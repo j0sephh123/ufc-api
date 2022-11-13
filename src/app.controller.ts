22e9235b3a78e3b7052a621726fdc1f3e7fa7be6
j0sephh123 <30373032+j0sephh123@users.noreply.github.com>
2022-11-13 11:39:34 +0200

    combine upcoming and upcoming matches in one endpoint (#9)

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,6 @@
 import { Controller, Get } from '@nestjs/common';
 import { SherdogService } from './sherdog.service';
-import { Match, UpcomingEvent } from './models';
+import { UpcomingEvent } from './models';
 import { ParserService } from './parser.service';
 
 @Controller('api/v1/events')
@@ -12,22 +12,15 @@ export class AppController {
 
   @Get('/upcoming')
   async upcomingEvent(): Promise<UpcomingEvent> {
-    const data = await this.sherdogService.events();
-    const parsedData = this.parserService.sherdogEvents(data);
+    const sherdogEventsHtml = await this.sherdogService.events();
+    const upcomingEvent = this.parserService.sherdogEvents(sherdogEventsHtml);
 
-    return parsedData;
-  }
-
-  @Get('/upcomingMatches')
-  async upcomingMatches(): Promise<{ matches: Match[] }> {
-    const eventsHtml = await this.sherdogService.events();
-    const upcomingEventUrl = this.parserService.upcomingEventUrl(eventsHtml);
     const upcomingEventHtml = await this.sherdogService.upcomingEvent(
-      upcomingEventUrl,
+      upcomingEvent.sherdogUrl,
     );
+    const upcomingEventMatches =
+      this.parserService.sherdogUpcomingMatches(upcomingEventHtml);
 
-    return {
-      matches: this.parserService.sherdogUpcomingMatches(upcomingEventHtml),
-    };
+    return { ...upcomingEvent, matches: upcomingEventMatches };
   }
 }
